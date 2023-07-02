dd11fa4250f80c888bda25aa2edc45a97e0a9710
Taliesin Millhouse <30204322+strongishllama@users.noreply.github.com>
2023-07-02 16:20:58 +1000

    feat: added ssh config

--- macos/install.sh
+++ macos/install.sh
@@ -2,6 +2,10 @@
 
 BASE_URL=~/Developer/personal/repositories/dotfiles/macos
 
+echo "Creating Git symlinks..."
 ln -sf $BASE_URL/git/.gitconfig ~/.gitconfig
 ln -sf $BASE_URL/git/.gitconfig.personal ~/Developer/personal/.gitconfig
 ln -sf $BASE_URL/git/.gitignore_global ~/.gitignore_global
+
+echo "Creating SSH symlinks..."
+ln -sf $BASE_URL/ssh/config ~/.ssh/config
