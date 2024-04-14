e02fe4aca6172615da96d1c999d48725f9942827
Taliesin Millhouse <taliesin@millhouse.dev>
2024-04-15 09:56:13 +1000

    feat: added script

--- macos/scripts/git-delete-branch.sh
+++ macos/scripts/git-delete-branch.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Full credit goes to: https://polothy.github.io/post/2019-08-19-fzf-git-checkout
-
 fzf-git-branch() {
     git rev-parse HEAD > /dev/null 2>&1 || return
 
@@ -20,11 +18,4 @@ if [[ "$branch" = "" ]]; then
     return
 fi
 
-# If branch name starts with 'remotes/' then it is a remote branch. By
-# using --track and a remote branch name, it is the same as:
-# git checkout -b branchName --track origin/branchName
-if [[ "$branch" = 'remotes/'* ]]; then
-    git checkout --track "$branch"
-else
-    git checkout "$branch";
-fi
+git branch -d "$branch";
