322c60d079227f9e8e651d8b7bfd9fe01fcd12de
Taliesin Millhouse <gitlab.saucy919@passmail.net>
2025-04-02 13:22:49 +1100

    updated neovim config

--- macos/nvim/init.lua
+++ macos/nvim/init.lua
@@ -644,6 +644,7 @@ require("lazy").setup({
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
+				bashls = {},
 				gopls = {
 					settings = {
 						gopls = {
@@ -661,20 +662,7 @@ require("lazy").setup({
 						jpath = jpaths,
 					},
 				},
-				-- pyright = {},
-				-- rust_analyzer = {},
-				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--
-				-- Some languages (like typescript) have entire language plugins that can be useful:
-				--    https://github.com/pmizio/typescript-tools.nvim
-				--
-				-- But for many setups, the LSP (`ts_ls`) will work just fine
-				-- ts_ls = {},
-				--
 				lua_ls = {
-					-- cmd = {...},
-					-- filetypes = { ...},
-					-- capabilities = {},
 					settings = {
 						Lua = {
 							completion = {
